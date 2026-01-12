b9ebf50bb1f99588ecc6ac839111a839bcc99663
Karl Hans Laurits <karl@laurits.dev>
2026-01-12 19:06:24 +0200

    Update .chezmoi.toml Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -4,9 +4,9 @@ if status is-interactive
     set --global --export EDITOR hx
     set --global --export VISUAL zed
     set --global --export PAGER bat
-    mise activate fish | source
     zoxide init fish --cmd cd | source
     starship init fish | source
+    atuin init fish | source
 end
 
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
