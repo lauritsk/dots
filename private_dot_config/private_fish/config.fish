0a49c5f00fed331265058e4a72e4697e8f1ee328
Karl Hans Laurits <karl@laurits.dev>
2026-01-13 02:38:33 +0200

    Add fnox.toml Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     zoxide init fish --cmd cd | source
     starship init fish | source
     atuin init fish | source
+    fnox activate fish | source
 end
 
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
