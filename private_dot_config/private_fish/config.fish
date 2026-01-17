0243ec042a758c456584f819228eb77298038ca8
Karl Hans Laurits <karl@laurits.dev>
2026-01-17 12:58:07 +0200

    Add .config/fish/config.fish

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    set -gx fish_greeting
+    set -gx fish_key_bindings fish_vi_key_bindings
+    eval (/opt/homebrew/bin/brew shellenv)
+    type -q mise; and mise activate fish | source
+    type -q starship; and starship init fish | source
+    type -q zoxide; and zoxide init fish --cmd cd | source
+    type -q atuin; and atuin init fish | source
+end
