d4129ba315e2e3ec7ff25d00433e345da5096bde
Karl Hans Laurits <karl@laurits.dev>
2026-01-20 01:26:45 +0200

    Update config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,6 +3,7 @@ if status is-interactive
     set -gx fish_greeting
     set -gx fish_key_bindings fish_vi_key_bindings
     type -q mise; and mise activate fish | source
+    type -q pitchfork; and pitchfork activate fish | source
     type -q starship; and starship init fish | source
     type -q zoxide; and zoxide init fish --cmd cd | source
     type -q atuin; and atuin init fish | source
